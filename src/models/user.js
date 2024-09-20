bd500bd9a651738506cdb7eb716ba1ec7ee20f44
codepushed <mehrashubham216@gmail.com>
2024-09-20 17:09:30 +0530

    pswd encrpt

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,11 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 module.exports = mongoose.model("User", userSchema);
