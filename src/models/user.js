dd893f1b1ea6ed7614c87f7b02456626ba49404a
codepushed <mehrashubham216@gmail.com>
2024-09-20 17:03:13 +0530

    user model

---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    validate: [validator.isEmail, "Please enter email in correct format"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minLength: [6, "Password should be atleast 6 character"],
+    select: false,
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  photo: {
+    id: {
+      type: String,
+      required: true,
+    },
+    secure_url: {
+      type: String,
+      required: true,
+    },
+  },
+  forgotPasswordToken: String,
+  forgotPasswordExpiry: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
