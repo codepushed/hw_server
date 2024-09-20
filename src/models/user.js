cf0034cc8a9ff9eff472e1fde6a3615ac5b3df2a
codepushed <mehrashubham216@gmail.com>
2024-09-20 17:17:14 +0530

    token signing

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,6 +49,12 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isValidatePassword = async function(userendPassword){
    return await bcrypt.compare(userendPassword, this.password)
+};
+
+userSchema.methods.getJwtToken = function(){
+  return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRY,
+  });
 }
 
 module.exports = mongoose.model("User", userSchema);
