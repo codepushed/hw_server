b61fc5001ef588450ba88323a47e5c51bb8d0476
codepushed <mehrashubham216@gmail.com>
2024-09-21 13:57:33 +0530

    password reset token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,7 @@ const BigPromise = require("../middlewares/bigPromise");
 const CustomError = require("../utils/customError");
 const cookieToken = require("../utils/cookieToken");
 const mailHelper = require("../utils/emailHelper");
+const crypto = require("crypto");
 
 exports.signup = BigPromise(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -20,76 +21,104 @@ exports.signup = BigPromise(async (req, res, next) => {
   cookieToken(user, res);
 });
 
-
 exports.login = BigPromise(async (req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
 
-  if(!email || !password){
-    return next(new CustomError('please provide email and password', 400))
+  if (!email || !password) {
+    return next(new CustomError("please provide email and password", 400));
   }
 
-  const user = await User.findOne({email}).select("+password")
+  const user = await User.findOne({ email }).select("+password");
 
-  if(!user){
-    return next(new CustomError('No account found', 400))
+  if (!user) {
+    return next(new CustomError("No account found", 400));
   }
 
-  const isPasswordCorrect = await user.isValidatePassword(password)
-  
-  if(!isPasswordCorrect){
-    return next(new CustomError('No account found', 400))
+  const isPasswordCorrect = await user.isValidatePassword(password);
+
+  if (!isPasswordCorrect) {
+    return next(new CustomError("No account found", 400));
   }
 
-  cookieToken(user, res)
+  cookieToken(user, res);
+});
 
-})
-
-exports.logout = BigPromise(async(req, res, next) => {
-  res.cookie('token', null, {
+exports.logout = BigPromise(async (req, res, next) => {
+  res.cookie("token", null, {
     expires: new Date(Date.now()),
-    httpOnly: true
-  })
+    httpOnly: true,
+  });
   res.status(400).json({
     success: true,
-    message: "logout success"
-  })
-})
+    message: "logout success",
+  });
+});
 
-
-exports.forgotPassword = BigPromise(async(req, res, next) => {
+exports.forgotPassword = BigPromise(async (req, res, next) => {
   const { email } = req.body;
 
-  const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-  if(!user) {
-    return next(new CustomError('Email not found', 400))
+  if (!user) {
+    return next(new CustomError("Email not found", 400));
   }
 
-  const forgotToken = user.getForgotPasswordToken()
+  const forgotToken = user.getForgotPasswordToken();
 
-  await user.save({validateBeforeSave: false})
+  await user.save({ validateBeforeSave: false });
 
-  const forgotUrl = `${req.protocol}://${req.get("host")}/password/reset${forgotToken}`
+  const forgotUrl = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/password/reset${forgotToken}`;
 
-  const message = `Copy paste this link in your browser to reset password \n \n ${forgotUrl}`
+  const message = `Copy paste this link in your browser to reset password \n \n ${forgotUrl}`;
 
-  try{ 
+  try {
     await mailHelper({
       email: user.email,
       subject: "Homework Service - Password reset email",
-      message
+      message,
     });
 
     res.status(200).json({
       success: true,
-      message: "Email sent successfully"
-    })
+      message: "Email sent successfully",
+    });
+  } catch (err) {
+    user.forgotPasswordToken = undefined;
+    user.forgotPasswordExpiry = undefined;
+    await user.save({ validateBeforeSave: false });
 
-  }catch(err){
-    user.forgotPasswordToken = undefined
-    user.forgotPasswordExpiry = undefined
-    await user.save({validateBeforeSave: false})
-
-    return next(new CustomError(err.message, 500))
+    return next(new CustomError(err.message, 500));
   }
-})
+});
+
+exports.forgotPasswordReset = BigPromise(async (req, res, next) => {
+  const token = req.params.token;
+
+  const encryptToken = crypto.createHash("sha256").update(token).digest("hex");
+
+  const user = await User.findOne({
+    encryptToken,
+    forgotPasswordExpiry: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return next(new CustomError("Token is invalid or expired", 400));
+  }
+
+  if (req.body.password !== req.body.confirmPassword) {
+    return next(
+      new CustomError("Password and confirm password do not match", 400)
+    );
+  }
+
+  user.password = req.body.password;
+
+  user.forgotPasswordToken = undefined;
+  user.forgotPasswordExpiry = undefined;
+
+  await user.save();
+
+  cookieToken(user, res);
+});
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const { signup, login, logout, forgotPassword } =  require("../controllers/userController");
+const {
+  signup,
+  login,
+  logout,
+  forgotPassword,
+  forgotPasswordReset,
+} = require("../controllers/userController");
 
-router.route('/signup').post(signup);
-router.route('/login').post(login);
-router.route('/logout').get(logout);
-router.route('/forgotpassword').post(forgotPassword);
+router.route("/signup").post(signup);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/forgotpassword").post(forgotPassword);
+router.route("/password/reset/:token").post(forgotPasswordReset);
 
 module.exports = router;
