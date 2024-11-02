7dd5ee365679002fb9c09e46b6f5e9104714a9f0
codepushed <mehrashubham216@gmail.com>
2024-11-02 12:11:43 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
@@ -7,6 +8,8 @@ const fileUpload = require('express-fileupload');
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+app.use(cors());
 
 app.use(cookieParser());
 app.use(fileUpload());
