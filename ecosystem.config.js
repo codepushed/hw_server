4d34fb4ccdafccdfc8de4b1c21dd476b62ecf481
codepushed <mehrashubham216@gmail.com>
2024-11-05 12:10:43 +0530

    env configs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,32 @@
 module.exports = {
-    apps: [
-      {
-        name: "app-staging",
-        script: "./server.js",
-        env: {
-          NODE_ENV: "staging",
-          MONGO_URI: "mongodb://localhost:27017/myapp_staging"
-        },
+  apps: [
+    {
+      name: "app-staging",
+      script: "./server.js",
+      env: {
+        NODE_ENV: "staging",
+        MONGO_URI: "mongodb://localhost:27017/myapp_staging",
+        COOKIE_TIME: "3",
+        JWT_SECRET: "thisismyjwttoken",
+        JWT_EXPIRY: "3d",
+        SMTP_PORT: "465",
+        SMTP_USER: "mehrashubham216@gmail.com",
+        SMTP_PASS: "ljpf hfhq vudg wekl",
       },
-      {
-        name: "app-production",
-        script: "./server.js",
-        env: {
-          NODE_ENV: "production",
-          MONGO_URI: "mongodb://localhost:27017/myapp_production"
-        },
+    },
+    {
+      name: "app-production",
+      script: "./server.js",
+      env: {
+        NODE_ENV: "production",
+        MONGO_URI: "mongodb://localhost:27017/myapp_production",
+        COOKIE_TIME: "3",
+        JWT_SECRET: "thisismyjwttoken",
+        JWT_EXPIRY: "3d",
+        SMTP_PORT: "465",
+        SMTP_USER: "mehrashubham216@gmail.com",
+        SMTP_PASS: "ljpf hfhq vudg wekl",
       },
-    ],
-  };
-  
+    },
+  ],
+};
