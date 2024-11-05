bfe7cb2cddf5f6422d0d0397c87e7ab5cbe21638
codepushed <mehrashubham216@gmail.com>
2024-11-05 06:46:47 +0000

    env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,22 @@ module.exports = {
     apps: [
       {
         name: "app-staging",
-        script: "./server.js",
+        script: "./index.js",
         env: {
           NODE_ENV: "staging",
-          MONGO_URI: "mongodb://localhost:27017/myapp_staging"
+          MONGO_URI: "mongodb://localhost:27017/myapp_staging",
+          script: '/var/www/hw_server/index.js',
+          PORT: 3001
         },
       },
       {
         name: "app-production",
-        script: "./server.js",
+        script: "./index.js",
         env: {
           NODE_ENV: "production",
-          MONGO_URI: "mongodb://localhost:27017/myapp_production"
+          MONGO_URI: "mongodb://localhost:27017/myapp_production",
+          script: '/var/www/hw_server/index.js',
+          PORT: 3002
         },
       },
     ],
